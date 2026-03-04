40cab582f912980f96c4a7cf64e89b643a6e0eb8
taupirho <thomas.reid@gmail.com>
2026-03-04 22:40:20 +0000

    Add startup message to app

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+st.write("✅ App started")
+
 st.set_page_config(page_title="SecureApp", page_icon="🔑", layout="wide")
 
 if "page" not in st.session_state:
